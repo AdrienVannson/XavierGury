4636242a72945b310da7eb776dc9221c5f1e6238
Adrien VANNSON <adrien.vannson@gmail.com>
2016-01-30 19:01:56 +0100

    Nouvelle présentation de la page de thème

--- view/theme.php
+++ view/theme.php
@@ -12,27 +12,55 @@ function show_theme($theme) {
 		<?php show_head($theme->get_name()); ?>
 		
 		<body>
-			<a href="/">Accueil</a>
-			<h1><?php echo $theme->get_name();?></h1>
-			<p><?php echo $theme->get_description();?></p>
 			
-			<h2>Projets :</h2>
+			<style>
+body {
+	background-color: #111;
+}
+
+
+#menu {
+	color: #FFF;
+	width: 18%;
+}
+
+#contents {
+	margin-left: 20%;
+	color: #EEE;
+}
+
+a {
+	color: #FFF;
+}
+			</style>
 			
-			<ul>
-				<?php 
-				$projects = $theme->get_projects();
-				
-				foreach($projects as $project) {
+			<div id="menu">
+				<ul>
+					<?php 
+					$projects = $theme->get_projects();
+					
+					foreach($projects as $project) {
+						?>
+						<li>
+							<a href="/<?php echo $theme->get_id();?>-<?php echo $theme->get_name_formatted();?>/<?php echo $project->get_id();?>-<?php echo $project->get_name_formatted();?>">
+								<?php echo strtoupper($project->get_name());?>
+							</a>
+						</li>
+						<?php
+					}
 					?>
-					<li>
-						<a href="/<?php echo $theme->get_id();?>-<?php echo $theme->get_name_formatted();?>/<?php echo $project->get_id();?>-<?php echo $project->get_name_formatted();?>">
-							<?php echo $project->get_name();?>
-						</a>
-					</li>
-					<?php
-				}
-				?>
-			</ul>
+				</ul>
+			</div>
+			
+			<div id="contents">
+				<a href="/">Accueil</a>
+				<h1><?php echo $theme->get_name();?></h1>
+				<p><?php echo $theme->get_description();?></p>
+				
+				<h2>Projets :</h2>
+				
+				
+			</div>
 			
 		</body>
 	</html>
