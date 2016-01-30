a4a5f12f36c0ff7a756e604a2ba774b200550e1f
Adrien VANNSON <adrien.vannson@gmail.com>
2016-01-30 18:49:05 +0100

    Ajout d'un head sur la page de theme

--- view/theme.php
+++ view/theme.php
@@ -1,30 +1,41 @@
 <?php
 /* View */
 include_once("../model/Theme.class.php");
+include_once("head.php");
 
 function show_theme($theme) {
 	?>
 	
-	<h1><?php echo $theme->get_name();?></h1>
-	<p><?php echo $theme->get_description();?></p>
-	
-	<h2>Projets :</h2>
-	
-	<ul>
-		<?php 
-		$projects = $theme->get_projects();
+	<!DOCTYPE HTML>
+	<html lang="fr">
 		
-		foreach($projects as $project) {
-			?>
-			<li>
-				<a href="/<?php echo $theme->get_id();?>-<?php echo $theme->get_name_formatted();?>/<?php echo $project->get_id();?>-<?php echo $project->get_name_formatted();?>">
-					<?php echo $project->get_name();?>
-				</a>
-			</li>
-			<?php
-		}
-		?>
-	</ul>
+		<?php show_head($theme->get_name()); ?>
+		
+		<body>
+			<a href="/">Accueil</a>
+			<h1><?php echo $theme->get_name();?></h1>
+			<p><?php echo $theme->get_description();?></p>
+			
+			<h2>Projets :</h2>
+			
+			<ul>
+				<?php 
+				$projects = $theme->get_projects();
+				
+				foreach($projects as $project) {
+					?>
+					<li>
+						<a href="/<?php echo $theme->get_id();?>-<?php echo $theme->get_name_formatted();?>/<?php echo $project->get_id();?>-<?php echo $project->get_name_formatted();?>">
+							<?php echo $project->get_name();?>
+						</a>
+					</li>
+					<?php
+				}
+				?>
+			</ul>
+			
+		</body>
+	</html>
 	
 	<?php
 }
