c1639d6aad73175999d9734b0738e870eb295344
Adrien VANNSON <adrien.vannson@gmail.com>
2016-02-03 19:31:56 +0100

    Déplacement du code d'affichage de la grille dans une fonction

--- view/themes/themes_script.php
+++ view/themes/themes_script.php
@@ -37,22 +37,24 @@ function isPossible(iLine, iColumn) {
 	return random < 0.1; // Environ 1/10 de chance
 }
 
-$(document).ready(function() {
+function createGride() {
+	$('#themes').html('');
 	
 	var themes = [
-	<?php
-	class ThemeShowed {
-		public function __construct($name, $url, $color) {
-			$this->name = $name;
-			$this->url = $url;
-			$this->color = $color;
-		}
-		public $name, $url, $color;
-	}
+	          	
+    <?php
+    class ThemeShowed {
+    	public function __construct($name, $url, $color) {
+    		$this->name = $name;
+    		$this->url = $url;
+    		$this->color = $color;
+    	}
+    	public $name, $url, $color;
+    }
 	$themesShowed = [];
 	
 	$iTheme = 0;
-	foreach($themes as $theme) {
+    foreach($themes as $theme) {
 		$themesShowed[] = new ThemeShowed($theme->get_name(), $theme->get_url(), $theme->get_color());
 	}
 	$themesShowed[] = new ThemeShowed("", "/", "FFF");
@@ -65,40 +67,43 @@ $(document).ready(function() {
 			echo ",";
 		}
 	}
-	
-	?>
-	];
-	
-	var width = $(document).width();
-	var nbColumns = Math.floor(width / 200);
+    ?>
+    ];
+          	
+    var width = $(document).width();
+    var nbColumns = Math.floor(width / 200);
 
-	var iLine=0;
-	while(themes.length>0 && iLine<100) {
+    var iLine=0;
+    while(themes.length>0 && iLine<100) {
 		addLine(iLine, nbColumns);
-		
+    	
 		for(var iColumn=0; iColumn<nbColumns; iColumn++) {
 
 			if(isPossible(iLine, iColumn)) {
 				var last = themes.pop();
-				
+          				
 				$('#column-'+iLine+'-'+iColumn)
 					.addClass('theme')
 					.css('backgroundColor', last.color)
 					.html('<div></div><a href="'+last.url+'">'+last.name+'</a>');
-				
+          				
 
 				if(themes.length == 0) {
 					break;
 				}
 			}
-			
 		}
 
 		iLine++;
-	}
+    }
 
 	$('.column').width('calc('+100/nbColumns+'% - 1px)');
 	$('.column:first-child').width('calc('+100/nbColumns+'% - 2px)');
+}
+
+$(document).ready(function() {
+	
+	createGride();
 
 });
 	<?php
