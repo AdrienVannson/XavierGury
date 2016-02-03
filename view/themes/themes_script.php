0c87ecc196499d36a9de4db0e8b8aaabf8708f31
Adrien VANNSON <adrien.vannson@gmail.com>
2016-02-03 17:32:32 +0100

    Coloration de cellules en rouge aléatoirement

--- view/themes/themes_script.php
+++ view/themes/themes_script.php
@@ -6,18 +6,45 @@ function show_theme_script() {
 /* Comment to enable the JS coloration in Eclipse
 <script>
 // */
+
+function addLine(iLine, nbColumns) {
+	$('#themes').append('<div class="line" id="line-'+iLine+'"></div>');
+
+	for(var iColumn=0; iColumn<nbColumns; iColumn++) {
+		$('#line-'+iLine).append('<div class="column" id="column-'+iLine+'-'+iColumn+'"></div>');
+	}
+}
+
+function isPossible(iLine, iColumn) {
+	
+	for(var iLineNextTo=iLine-1; iLineNextTo<=iLine+1; iLineNextTo++) {
+		for(var iColumnNextTo=iColumn-1; iColumnNextTo<=iColumn+1; iColumnNextTo++) {
+			
+			if( $('#column-'+iLineNextTo+'-'+iColumnNextTo).hasClass('theme') ) {
+				return false;
+			}
+			
+		}
+	}
+	
+	var random = Math.random();
+	return random < 0.1; // Environ 1/10 de chance
+}
+
 $(document).ready(function() {
 	
 	var width = $(document).width();
 	var nbColumns = Math.floor(width / 200);
 	
 	for(var iLine=0; iLine<10; iLine++) {
-
-		$("#themes").append('<div class="line" id="line-'+iLine+'"></div>');
+		addLine(iLine, nbColumns);
 		
 		for(var iColumn=0; iColumn<nbColumns; iColumn++) {
-			
-			$("#line-"+iLine).append('<div class="column" id="column-'+iLine+'-'+iColumn+'"></div>');
+
+			if(isPossible(iLine, iColumn)) {
+				$('#column-'+iLine+'-'+iColumn).addClass('theme');
+				$('#column-'+iLine+'-'+iColumn).css('backgroundColor', 'red');
+			}
 			
 		}
 		
