4097e15a5980c9216793be3eaf7cdb7f9b6a1505
Adrien VANNSON <adrien.vannson@gmail.com>
2016-02-08 14:29:20 +0100

    Ajout d'antislashs pour protéger les noms de thèmes utilisés par JS lorsqu'ils contiennent des apostrophes

--- view/themes/themes_script.php
+++ view/themes/themes_script.php
@@ -61,7 +61,7 @@ function createGride() {
 	shuffle($themesShowed);
 	
 	foreach($themesShowed as $theme) {
-		echo "new Theme('".mb_strtoupper($theme->name)."', '".$theme->url."', '#".$theme->color."')";
+		echo "new Theme('".str_replace("'", "\\'", mb_strtoupper($theme->name))."', '".$theme->url."', '#".$theme->color."')";
 		
 		if($iTheme < sizeof($themesShowed)-1) {
 			echo ",";
