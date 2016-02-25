9a80eb17cae7c18e6d06742079d46886502202a6
Adrien VANNSON <adrien.vannson@gmail.com>
2016-02-25 19:15:24 +0100

    Possibilité de créer des images sans les enregistrer

--- model/Picture.class.php
+++ model/Picture.class.php
@@ -7,34 +7,75 @@ class Picture {
 	public function __construct($id) {
 		$db = get_db();
 		
-		if($id == -1) { /* Ajout d'une nouvelle ressource */
-			$results = $db->prepare("INSERT INTO pictures () VALUES ();");
+		if($id == -1) {
+			/*$results = $db->prepare("INSERT INTO pictures () VALUES ();");
 			$results->execute(array());
 			
 			$results = $db->query("SELECT LAST_INSERT_ID() AS id");
 			$datas = $results->fetch();
-			$id = $datas["id"];
+			$id = $datas["id"];*/
+			
+			$this->id = -1;
+			$this->idProject;
+			$this->name = "";
+			$this->description = "";
+		}
+		else {
+			$results = $db->prepare("SELECT * FROM pictures WHERE id=?");
+			$results->execute(array($id));
+			$datas = $results->fetch();
+			
+			$this->id = $datas["id"];
+			$this->idProject = $datas["id_project"];
+			$this->name = $datas["name"];
+			$this->description = $datas["description"];
+		}
+	}
+	
+	
+	public function save() {
+		$db = get_db();
+		
+		if($this->id == -1) {
+			$results = $db->prepare("INSERT INTO pictures (id_project, name, description) VALUES (?, ?, ?);");
+			$results->execute(array(
+					$this->idProject,
+					$this->name,
+					$this->description
+			));
+				
+			$results = $db->query("SELECT LAST_INSERT_ID() AS id");
+			$datas = $results->fetch();
+			$this->id = $datas["id"];
+		}
+		else {
+			$results = $db->prepare("UPDATE pictures SET id_project=?, name=?, description=? WHERE id=?;");
+			$results->execute(array(
+					$this->idProject,
+					$this->name,
+					$this->description,
+					$this->id
+			));
+		}
+	}
+	
+	public function delete() {
+		if($this->id == -1) {
+			return;
 		}
 		
-		$results = $db->prepare("SELECT * FROM pictures WHERE id=?");
-		$results->execute(array($id));
-		$datas = $results->fetch();
-		
-		$this->id = $datas["id"];
-		$this->idProject = $datas["id_project"];
-		$this->name = $datas["name"];
-		$this->description = $datas["description"];
+		$db = get_db();
+	
+		$results = $db->prepare("DELETE FROM pictures WHERE id=?");
+		$results->execute(array($this->id));
 	}
+	
 	
 	public function get_id() {
 		return $this->id;
 	}
 	
 	public function set_id_project($idProject) {
-		$db = get_db();
-		$results = $db->prepare("UPDATE pictures SET id_project=? WHERE id=?;");
-		$results->execute(array($idProject, $this->id));
-		
 		$this->idProject = $idProject;
 	}
 	public function get_id_project() {
@@ -42,10 +83,6 @@ class Picture {
 	}
 	
 	public function set_name($name) {
-		$db = get_db();
-		$results = $db->prepare("UPDATE pictures SET name=? WHERE id=?;");
-		$results->execute(array($name, $this->id));
-	
 		$this->name = $name;
 	}
 	public function get_name() {
@@ -53,15 +90,12 @@ class Picture {
 	}
 	
 	public function set_description($description) {
-		$db = get_db();
-		$results = $db->prepare("UPDATE pictures SET description=? WHERE id=?;");
-		$results->execute(array($description, $this->id));
-		
 		$this->description = $description;
 	}
 	public function get_description() {
 		return $this->description;
 	}
+	
 	
 	public function get_path_resource($size) {
 		return "resources/pictures/".$this->id."/".$size.".jpg";
