002bb7999d5165f2b3b575b0e2d68dc48e4ab392
Adrien VANNSON <adrien.vannson@gmail.com>
2016-02-27 18:19:03 +0100

    Modification de la position du menu indicant l'arborescence

--- admin/view/menus.php
+++ admin/view/menus.php
@@ -13,21 +13,6 @@ function show_admin_menus($path=array()) {
 			<li><a href="/">Site</a></li>
 		</ul>
 		
-		<nav>
-			<div class="nav-wrapper">
-				<div class="col s12">
-					<a href="/admin/" class="breadcrumb">Administration</a>
-					<?php
-					foreach($path as $name) {
-						?>
-						<a href="<?php echo $name[1];?>" class="breadcrumb"><?php echo $name[0];?></a>
-						<?php
-					}
-					?>
-				</div>
-			</div>
-		</nav>
-		
 	</div>
 </nav>
 
@@ -39,6 +24,22 @@ function show_admin_menus($path=array()) {
 <a href="#" data-activates="slide-out" class="button-collapse">
 	<i class="mdi-navigation-menu" style="font-size:40px;color:#FFF;position:absolute;top:0;"></i>
 </a>
+
+
+<nav style="background-color: rgba(238, 110, 115, 0.85);">
+	<div class="container nav-wrapper">
+		<div class="col s12">
+			<a href="/admin/" class="breadcrumb">Administration</a>
+			<?php
+			foreach($path as $name) {
+				?>
+				<a href="<?php echo $name[1];?>" class="breadcrumb"><?php echo $name[0];?></a>
+				<?php
+			}
+			?>
+		</div>
+	</div>
+</nav>
 
 	<?php
 }
