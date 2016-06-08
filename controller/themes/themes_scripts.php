9fce0919a36ea4a003085bfc1e131b57c13e57de
Adrien VANNSON <adrien.vannson@gmail.com>
2016-06-08 18:04:46 +0200

    Affichage de la page des thèmes

--- controller/themes/themes_scripts.php
+++ controller/themes/themes_scripts.php
@@ -1,10 +1,8 @@
 <?php
 /* Controller */
-include_once(__DIR__."/../../model/Theme.class.php");
-include_once(__DIR__."/../../model/themes.php");
+include_once(__DIR__."/../../model/Project.class.php");
 include_once(__DIR__."/../../view/themes/themes_script.php");
 
 header("Content-Type: text/js");
 
-$themes = get_themes();
-show_theme_script($themes);
+show_theme_script( get_first_level_projects() );
--- view/themes.php
+++ view/themes.php
@@ -1,6 +1,5 @@
 <?php
 /* View */
-include_once(__DIR__."/../model/Theme.class.php");
 include_once(__DIR__."/head.php");
 
 function show_themes() {
