7a9e95559a2246dfe97724b021304edf30384d78
Adrien VANNSON <adrien.vannson@gmail.com>
2016-07-09 20:14:41 +0200

    Affichage dynamique de l'image de la page d'accueil

--- controller/homepage.php
+++ controller/homepage.php
@@ -1,5 +1,6 @@
 <?php
 /* Controller */
 include_once(__DIR__."/../view/homepage.php");
+include_once(__DIR__."/../model/Project.class.php");
 
-show_homepage();
+show_homepage(get_first_level_projects());
