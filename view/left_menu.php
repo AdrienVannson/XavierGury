5f944e03d0e7474078475f7a74a7695a1bb7c596
Adrien VANNSON <adrien.vannson@gmail.com>
2016-07-10 11:27:38 +0200

    Correction de l'indentation dans certains fichiers

--- view/left_menu.php
+++ view/left_menu.php
@@ -6,78 +6,72 @@ function show_left_menu($id_project) {
 	$project = new Project($id_project);
 	
 	?>
-	
-	
-	<div id="menu">
-		<ul>
-			<?php 
-			if ($project->get_id_parent() == 0) {
-				$projects = $project->get_children();
-			}
-			else {
-				$projects = $project->get_brothers();
-			}
-			
-			foreach($projects as $currentProject) {
-				?>
-				<li>
-					<a 
-						href="<?php echo $currentProject->get_url();?>"
-						<?php if($currentProject->get_id()==$id_project){?>class="active"<?php }?>
-					>
-						<?php echo mb_strtoupper($currentProject->get_name(), "UTF-8");?>
-					</a>
-				</li>
-				<?php
-			}
-			?>
-		</ul>
-		
-		<div id="color_menu">
-			
-			<?php 
-			foreach(get_first_level_projects() as $currentProject) {
-				?>
-				<a
-					<?php if($project->get_id()==$currentProject->get_id() || $project->get_id_parent()==$currentProject->get_id()){?>id="color_item_activate"<?php }?>
-					class="color_item"
-					style="background-color: #<?php echo $currentProject->get_color();?>;"
-					href="<?php echo $currentProject->get_url();?>"
-				></a>
-				<?php
-			}
-			?>
-			
-		</div>
-	</div>
-	
-	<div class="line" id="line-menu"></div>
-	<div class="line" id="line-bottom"></div>
-	<div class="line" id="line-white-item-1"></div>
-	<div class="line" id="line-white-item-2"></div>
-	
-	<div id="separator-bottom"></div>
-	
-	<a id="white-item" href="/"></a>
-	
-	<div id="menu-btn" onclick="
-		if(document.body.id=='') {
-			document.body.id = 'deplie';
+
+
+<div id="menu">
+	<ul>
+		<?php 
+		if ($project->get_id_parent() == 0) {
+			$projects = $project->get_children();
 		}
 		else {
-			document.body.id = '';
+			$projects = $project->get_brothers();
 		}
-	">
-		<svg style="width:48px;height:48px" viewBox="0 0 24 24">
-			<path fill="#FFF" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
-		</svg>
+
+		foreach($projects as $currentProject) {
+			?>
+			<li>
+				<a 
+					href="<?php echo $currentProject->get_url();?>"
+					<?php if($currentProject->get_id()==$id_project){?>class="active"<?php }?>
+				>
+					<?php echo mb_strtoupper($currentProject->get_name(), "UTF-8");?>
+				</a>
+			</li>
+			<?php
+		}
+		?>
+	</ul>
+
+	<div id="color_menu">
+
+		<?php 
+		foreach(get_first_level_projects() as $currentProject) {
+			?>
+			<a
+				<?php if($project->get_id()==$currentProject->get_id() || $project->get_id_parent()==$currentProject->get_id()){?>id="color_item_activate"<?php }?>
+				class="color_item"
+				style="background-color: #<?php echo $currentProject->get_color();?>;"
+				href="<?php echo $currentProject->get_url();?>"
+			></a>
+			<?php
+		}
+		?>
+
 	</div>
-	
-	<script>
+</div>
 
-	
+<div class="line" id="line-menu"></div>
+<div class="line" id="line-bottom"></div>
+<div class="line" id="line-white-item-1"></div>
+<div class="line" id="line-white-item-2"></div>
 
-	</script>
-	
+<div id="separator-bottom"></div>
+
+<a id="white-item" href="/"></a>
+
+<div id="menu-btn" onclick="
+	if(document.body.id=='') {
+		document.body.id = 'deplie';
+	}
+	else {
+		document.body.id = '';
+	}
+">
+	<svg style="width:48px;height:48px" viewBox="0 0 24 24">
+		<path fill="#FFF" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+	</svg>
+</div>
+
 	<?php
 }
--- view/project.php
+++ view/project.php
@@ -11,84 +11,85 @@ function show_project($project) {
 	
 <!DOCTYPE HTML>
 <html lang="fr">
-	<?php show_head($project->get_name(), array("/styles/project.css")); ?>
+<?php show_head($project->get_name(), array("/styles/project.css")); ?>
+<body>
 
-	<body>
-
-		<?php show_left_menu($project->get_id()); ?>
-
-		<div id="contents">
-
-			<h1><?php echo $project->get_name();?></h1>
+<?php show_left_menu($project->get_id()); ?>
 
 
-<?php
+<div id="contents">
 
-$children = $project->get_children();
+	<h1><?php echo $project->get_name();?></h1>
 
-if (sizeof($children)) {
-	
-	foreach ($children as $child) {
-		?>
-		
-			<div class="under-project">
-				<h2><a href="<?php echo $child->get_url();?>"><?php echo $child->get_name();?></a></h2>
-				<p><?php echo $child->get_description();?></p>
-			</div>
-		
-		<?php
+
+	<?php
+
+	$children = $project->get_children();
+
+	if (sizeof($children)) {
+
+		foreach ($children as $child) {
+			?>
+
+				<div class="under-project">
+					<h2><a href="<?php echo $child->get_url();?>"><?php echo $child->get_name();?></a></h2>
+					<p><?php echo $child->get_description();?></p>
+				</div>
+
+			<?php
+		}
+
 	}
-	
-}
 
-?>
-			
-			<p>
-				<?php
-				$pictures = $project->get_pictures();
-				foreach($pictures as $picture) {
-					?>
-					<img 
-						 src="<?php echo $picture->get_url_resource("medium");?>"
-						 class="project-picture"
-						 title="<?php echo $picture->get_name();?>"
-						 alt="<?php echo $picture->get_description();?>"
-						 onclick="showPicture(this)"
-					/>
-					<?php
-				}
-				?>
-			</p>
+	?>
 
-			<div class="description"><?php echo $project->get_description();?></div>
+	<p>
+		<?php
+		$pictures = $project->get_pictures();
+		foreach($pictures as $picture) {
+			?>
+			<img 
+				 src="<?php echo $picture->get_url_resource("medium");?>"
+				 class="project-picture"
+				 title="<?php echo $picture->get_name();?>"
+				 alt="<?php echo $picture->get_description();?>"
+				 onclick="showPicture(this)"
+			/>
+			<?php
+		}
+		?>
+	</p>
 
+	<div class="description"><?php echo $project->get_description();?></div>
+
+</div>
+
+
+<div id="picture-preview">
+
+	<svg id="close" viewBox="0 0 24 24" fill="#FFF" onclick="closePreview();">
+		<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+		<path d="M0 0h24v24H0z" fill="none"/>
+	</svg>
+
+	<div id="frame">
+
+		<div id="picture-container">
+			<div><img id="picture"/></div>
 		</div>
-		
-		<div id="picture-preview">
-			
-			<svg id="close" viewBox="0 0 24 24" fill="#FFF" onclick="closePreview();">
-				<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-				<path d="M0 0h24v24H0z" fill="none"/>
-			</svg>
-			
-			<div id="frame">
-				
-				<div id="picture-container">
-					<div><img id="picture"/></div>
-				</div>
-				
-				<div id="informations">
-					<h1 id="title">TITRE</h1>
-					<p id="description"></p>
-				</div>
-				
-			</div>
-		</div>
-		
-		
-		<script type="text/javascript" src="/scripts/project.js"> </script>
 
-	</body>
+		<div id="informations">
+			<h1 id="title"></h1>
+			<p id="description"></p>
+		</div>
+
+	</div>
+</div>
+
+
+<script type="text/javascript" src="/scripts/project.js"> </script>
+
+</body>
 </html>
 
 <?php
