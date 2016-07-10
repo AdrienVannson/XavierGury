a5e3609ece4d77ad66ec9b7e64a8cc8736b17428
Adrien VANNSON <adrien.vannson@gmail.com>
2016-07-10 13:09:54 +0200

    Modification des changements récents

--- admin/view/homepage.php
+++ admin/view/homepage.php
@@ -46,6 +46,7 @@ function show_admin_homepage() {
 							<div class="card-content white-text">
 								<span class="card-title">Changements récents</span>
 								<ul>
+									<li>10 juillet 2016 : Possibilité de définir des sous-projets</li>
 									<li>15 juin 2016 : Correction de bugs</li>
 									<li>10 juin 2016 : Modification de la structure de la base de données</li>
 								</ul>
