eb762fd61eb145c0a672cf9d2340f4d6ba490ff2
Adrien VANNSON <adrien.vannson@gmail.com>
2016-08-24 12:43:04 +0200

    Renommage de left_menu.php en left-menu.php

--- view/project.php
+++ view/project.php
@@ -3,7 +3,7 @@
 include_once(__DIR__.'/../model/Project.class.php');
 include_once(__DIR__.'/../model/Picture.class.php');
 include_once(__DIR__.'/head.php');
-include_once(__DIR__.'/left_menu.php');
+include_once(__DIR__.'/left-menu.php');
 
 $project = $PROJECT;
 ?>
