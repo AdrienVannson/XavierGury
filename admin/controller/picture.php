e811519ff497489ff7c3a53676240a08bcfc4ad0
Adrien VANNSON <adrien.vannson@gmail.com>
2016-08-29 21:21:35 +0200

    Utilisation du model pour générer les aperçus lors de l'envoi de nouvelles images

--- admin/controller/picture.php
+++ admin/controller/picture.php
@@ -32,15 +32,19 @@ if (isset($_POST['save'])) {
 		$picture->save();
 
 		$error = $_FILES['image']['error'];
+		
 		if ($error == UPLOAD_ERR_INI_SIZE || $error == UPLOAD_ERR_FORM_SIZE) {
-			$_SESSION['errors'][] = 'Le fichier envoyé est trop lourd.';
+			$_SESSION['errors'][] = 'Le fichier envoyé est trop lourd';
 		}
+		
 		if ($error == UPLOAD_ERR_PARTIAL) {
-			$_SESSION['errors'][] = 'Le fichier n\'a pas été envoyé entièrement.';
+			$_SESSION['errors'][] = 'Le fichier n\'a pas été envoyé entièrement';
 		}
+		
 		if ($error ==  UPLOAD_ERR_CANT_WRITE) {
-			$_SESSION['errors'][] = 'Une erreur s\'est produite lors de l\'enregistrement du fichier sur le disque.';
+			$_SESSION['errors'][] = 'Une erreur s\'est produite lors de l\'enregistrement du fichier sur le disque';
 		}
+		
 		if (isset($_FILES['image']) && $_FILES['image']['error']==UPLOAD_ERR_OK) {
 
 			$dirName = dirname(dirname(__DIR__)).'/resources/pictures/'.$picture->getId();
@@ -49,76 +53,17 @@ if (isset($_POST['save'])) {
 			if (!file_exists($dirName)) {
 				mkdir($dirName, 0777);
 			}
-
-			// Vidage du dossier
-			$dir = opendir($dirName);
-			while ($file = readdir($dir)) {
-				if ($file != '.' && $file != '..') {
-					$toDel = $dirName.'/'.$file;
-					unlink($toDel);
-				}
-			}
-			closedir($dir);
-
-			$tmpName = $_FILES['image']['tmp_name'];
-
-			if ($_FILES['image']['type'] == 'image/jpeg') {
-				$img = imagecreatefromjpeg($tmpName);
-				$picture->setType('JPG');
-
-				$infos = getimagesize($_FILES['image']['tmp_name']);
-				$width = $infos[0];
-				$height = $infos[1];
-
-				$small = imagescale($img, floor(128*$width/$height));
-				$medium = imagescale($img, floor(512*$width/$height));
-
-				imagejpeg($small, $dirName.'/small.jpg', 75);
-				imagejpeg($medium, $dirName.'/medium.jpg', 80);
-				imagejpeg($img, $dirName.'/large.jpg', 100);
-			}
-			elseif ($_FILES['image']['type'] == 'image/gif') {
-				$img = imagecreatefromgif($tmpName);
-				$picture->setType('GIF');
-
-				$infos = getimagesize($_FILES['image']['tmp_name']);
-				$width = $infos[0];
-				$height = $infos[1];
-
-				$small = imagescale($img, floor(128*$width/$height));
-				$medium = imagescale($img, floor(512*$width/$height));
-
-				imagegif($small, $dirName.'/small.gif');
-				imagegif($medium, $dirName.'/medium.gif');
-				imagegif($img, $dirName.'/large.gif');
-			}
-			elseif ($_FILES['image']['type'] == 'image/png') {
-				$img = imagecreatefrompng($tmpName);
-				$picture->setType('PNG');
-
-				$infos = getimagesize($_FILES['image']['tmp_name']);
-				$width = $infos[0];
-				$height = $infos[1];
-
-				$small = imagescale($img, floor(128*$width/$height));
-				$medium = imagescale($img, floor(512*$width/$height));
-
-				imagealphablending($img, false);
-				imagesavealpha($img, true);
-				imagealphablending($small, false);
-				imagesavealpha($small, true);
-				imagealphablending($medium, false);
-				imagesavealpha($medium, true);
-
-				imagepng($small, $dirName.'/small.png', 7);
-				imagepng($medium, $dirName.'/medium.png', 8);
-
-				imagepng($img, $dirName.'/large.png', 9);
-			}
-			else {
+			
+			if ($_FILES['image']['type'] != 'image/gif' &&
+			    $_FILES['image']['type'] != 'image/png' &&
+			    $_FILES['image']['type'] != 'image/jpeg') {
+				
 				$_SESSION['errors'][] = 'Le format de fichier n\'est pas pris en charge';
 				goto end;
 			}
+			
+			move_uploaded_file($_FILES['image']['tmp_name'], $dirName.'/r.'.$picture->getType());
+			$picture->generateFiles();
 		}
 	}
 	
