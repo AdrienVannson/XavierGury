571b70f89704e52636ae5be19ab0da7a95d74eee
Adrien VANNSON <adrien.vannson@gmail.com>
2017-01-21 17:35:26 +0100

    Suppression de code inutile

--- admin/view/styles.php
+++ admin/view/styles.php
@@ -23,32 +23,5 @@ header, main, footer, nav {
 	font-family: ;
 }
 
-/*body {
-	background-color: #2B2E34;
-	color: #FFF;
-}
-
-a {
-	color: #FFF;
-	text-decoration: none;
-}
-a:hover {
-	text-decoration: underline;
-}
-
-/* Formulaires *//*
-label {
-	display: inline-block;
-	width: 300px;
-}
-
-
-/* Résultats *//*
-#message {
-	display: inline-block;
-	margin-left: 30px;
-	font-style: italic;
-}*/
-
 	<?php
 }
