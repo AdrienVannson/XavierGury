62e51e2ba3e3b5ee90ff9f09af07bb5b4316554d
Adrien VANNSON <adrien.vannson@gmail.com>
2017-11-30 19:51:48 +0100

    Masquage des flèches de l'aperçut si le projet ne comporte qu'une seule image

--- view/project/ProjectView.class.php
+++ view/project/ProjectView.class.php
@@ -284,17 +284,22 @@ class ProjectView extends HTMLView
                 <div id="frame">
                     <div id="picture-container" class="unselectable">
 
-                        <!-- Previous picture -->
-                        <svg fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="arrow" onclick="previousPicture()">
-                            <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
-                            <path d="M0 0h24v24H0z" fill="none"/>
-                        </svg>
+                        <?php
+                        // Masquer les flèches si il n'y a qu'une seule image
+                        if (sizeof($this->project->getPictures()) > 1) {
+                        ?>
+                            <!-- Previous picture -->
+                            <svg fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="arrow" onclick="previousPicture()">
+                                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
+                                <path d="M0 0h24v24H0z" fill="none"/>
+                            </svg>
 
-                        <!-- Next picture -->
-                        <svg fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="arrow" id="arrow-right" onclick="nextPicture()">
-                            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
-                            <path d="M0 0h24v24H0z" fill="none"/>
-                        </svg>
+                            <!-- Next picture -->
+                            <svg fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="arrow" id="arrow-right" onclick="nextPicture()">
+                                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+                                <path d="M0 0h24v24H0z" fill="none"/>
+                            </svg>
+                        <?php }  ?>
 
                     </div>
                     <div id="informations"></div>
