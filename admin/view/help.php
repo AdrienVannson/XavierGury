844355dd2f337ac83b85b230076d64060cd168f2
Adrien VANNSON <adrien.vannson@gmail.com>
2017-12-29 15:05:18 +0100

    Modification de la page d'aide

--- admin/view/help.php
+++ admin/view/help.php
@@ -5,44 +5,59 @@ include_once(__DIR__.'/menus.php');
 
 <!DOCTYPE HTML>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8"/>
+<head>
+<meta charset="utf-8"/>
 
-		<title>Aide - Administration - Xavier Gury</title>
+<title>Aide - Administration - Xavier Gury</title>
 
-		<link rel="icon" type="image/png" href="/favicon.png"/>
-		<link rel="stylesheet" type="text/css" href="/admin/styles.css"/>
-		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css"/>
+<link rel="icon" type="image/png" href="/favicon.png"/>
+<link rel="stylesheet" type="text/css" href="/admin/styles.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css"/>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	</head>
-	
-	<body>
-		<?php show_admin_menus();?>
-		
-		<main>
-			
-			<div class="container">
-				<h1>Aide</h1>
-				
-				<p>
-					<div class="col s12">
-						<a class="btn-large waves-effect waves-light" href="https://docs.google.com/document/d/1hg9tuYOozgpznKPkMDH__L1XOE5c9VzeFjym_ypg2Hk/">
-							Ajouter une vidéo
-						</a>
-					</div>
-				</p>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+<?php show_admin_menus();?>
+
+<main class="container">
+
+	<h1>Aide</h1>
+
+	<div class="row">
+
+		<div class="col s12">
+			<div class="card green hoverable">
+				<div class="card-content white-text">
+					<span class="card-title">Ajouter une vidéo</span>
+
+					<ul>
+						<li>Connectez-vous sur le compte Google du site</li>
+						<li>Rendez-vous à cette adresse : <a href="https://www.youtube.com/upload">https://www.youtube.com/upload</a></li>
+						<li>Publiez la vidéo sur Youtube</li>
+						<li>Créer une nouvelle vidéo depuis l’espace administration du site</li>
+						<li>Dans le champ “Code”, copiez-collez le code de la vidéo</li>
+					</ul>
+
+					<p>Le code de la vidéo est visible dans l'adresse de celle-ci, après watch?v=<br/>
+					Par exemple, pour cette vidéo :<br/>
+					https://www.youtube.com/watch?v=BPNTC7uZYrI<br/>
+					Le code est : BPNTC7uZYrI</p>
+
+				</div>
 			</div>
-			
-		</main>
+		</div>
+	</div>
+
+</main>
 
 
-		<script src="/static/jquery-3.2.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+<script src="/static/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
 
-		<script>
-		$('.button-collapse').sideNav();
-		</script>
-		
-	</body>
+<script>
+$('.button-collapse').sideNav();
+</script>
+
+</body>
 </html>
