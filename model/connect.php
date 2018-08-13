42a98c2c0ef145e5f94f22bc7c50863d5cf3f0aa
Adrien VANNSON <adrien.vannson@gmail.com>
2018-08-13 23:27:26 +0200

    Correction d'une inclusion

--- model/connect.php
+++ model/connect.php
@@ -1,6 +1,5 @@
 <?php
-
-include_once('../config.php');
+include_once(__DIR__.'/../config.php');
 
 try {
 	$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
